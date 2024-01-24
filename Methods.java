a07197937112409c6714994e3f159525fe4ba923
Srinivask0429 <jangapellisrinivas32@gmail.com>
2024-01-24 17:45:23 +0530

    this is java method file

---
+++ Methods.java
@@ -0,0 +1,30 @@
+package methods;
+public class Basicmethod 
+{
+	public static void main(String[] args) 
+	{
+		B b1;//referal variable
+		b1=new B();
+		b1.countryIndia();
+		b1.countryAustralia();
+		b1.countryRussia();
+	}
+}
+	class B
+	{
+		public void countryIndia()
+		{
+			System.out.println("INDIA");
+		}
+		public void countryAustralia()
+		{
+			System.out.println("Australia");
+		}
+		public void countryRussia()
+		{
+			System.out.println("RUSSIA");
+		}
+	}
+
+here u have to edit man 
+delete this line ..... sri
